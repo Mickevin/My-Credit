0cedcd2a6562c8e29d9373dc7a6a897c97c76022
Kévin Duranty <70485959+Quera-fr@users.noreply.github.com>
2023-10-04 16:23:45 +0200

    Update app.py

---
+++ app.py
@@ -0,0 +1,10 @@
+import streamlit as st
+
+st.set_page_config(
+    page_title="My Credit App", 
+    page_icon=":smiley:")
+
+st.title("My Credit App")
+
+st.text_input("Name")
+st.text_input("Age")
